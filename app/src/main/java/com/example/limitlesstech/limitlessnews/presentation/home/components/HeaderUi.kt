736d5708e172936357abea93bb6d7fa0bca6d4c0
Rishabh Pandey <rishabhpandey139@gmail.com>
2026-08-27 11:45:59 +0530

    Add default profile icon to home screen

--- app/src/main/java/com/example/limitlesstech/limitlessnews/presentation/home/components/HeaderUi.kt
+++ app/src/main/java/com/example/limitlesstech/limitlessnews/presentation/home/components/HeaderUi.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -64,18 +67,30 @@ fun HeaderSection(
                 onClick = { }
             ) {
 
-                AsyncImage(
-                    model = profileImageUrl.ifBlank { null },
-                    contentDescription = "Profile Image",
-                    modifier = Modifier
-                        .size(bgSize)
-                        .clip(CircleShape)
-                        .background(
-                            Color.White,
-                            CircleShape
-                        ),
-                    contentScale = ContentScale.Crop
-                )
+                if (profileImageUrl.isBlank()) {
+
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "Default Profile",
+                        modifier = Modifier.size(bgSize),
+                        tint = Color.Gray
+                    )
+
+                } else {
+
+                    AsyncImage(
+                        model = profileImageUrl,
+                        contentDescription = "Profile Image",
+                        modifier = Modifier
+                            .size(bgSize)
+                            .clip(CircleShape)
+                            .background(
+                                Color.White,
+                                CircleShape
+                            ),
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
         }
     }
